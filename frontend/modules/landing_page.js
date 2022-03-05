62e207f9bd84c3696208dd5d2ef1a13becd47037
CrioUser <crio-user@criodo.com>
2022-03-05 20:27:13 +0530

    ME_QTRIPDYNAMIC_MODULE_CITIES

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -2,12 +2,12 @@ import config from "../conf/index.js";
 
 
 async function init() {
-  console.log("From init()")
-  console.log(config.backendEndpoint)
+  // console.log("From init()")
+  // console.log(config.backendEndpoint)
 
   //Fetches list of all cities along with their images and description
   let cities = await fetchCities();
-    console.log(cities)
+    // console.log(cities)
   //Updates the DOM with the cities
   cities.forEach((key) => {
     addCityToDOM(key.id, key.city, key.description, key.image);
